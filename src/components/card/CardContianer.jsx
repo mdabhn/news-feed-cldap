05bfd9e803aa644b996edc20b3d7b951739d7d26
Hossain <md.abhn@gmail.com>
2022-08-19 23:49:20 +0600

    Home Page Added.

---
+++ src/components/card/CardContianer.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Badge from 'react-bootstrap/Badge'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+
+const CardContianer = ({ section, title, abstract, url, multimedia }) => {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Badge className='absolute right-2 top-2' bg={'info'}>
+        {section}
+      </Badge>
+      <Card.Img variant='top' src={multimedia[1].url} alt={section} />
+
+      <Card.Body>
+        <Card.Title title={title}>
+          {title.lenght < 15 ? title : title.slice(0, 20) + '...'}
+        </Card.Title>
+        <Card.Text
+          className='text-muted'
+          style={{ minHeight: '100px' }}
+          title={abstract}
+        >
+          {abstract
+            ? abstract.length < 100
+              ? abstract
+              : abstract.slice(0, 100) + '...'
+            : 'No details available...'}
+        </Card.Text>
+        <Button variant='dark' className='text-green-900 w-full mt-6'>
+          Read Deatils
+        </Button>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default CardContianer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import CardContianer from '../components/card/CardContianer'
 
 const Home = () => {
-  return <div>Home</div>
+  const [homeData, setHomeData] = useState([])
+
+  useEffect(() => {
+    fetch(
+      `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        if (res?.status) {
+          console.log(res)
+          if (res.status === 'OK') {
+            setHomeData(res.results)
+            localStorage.setItem('nf-home', JSON.stringify(res.results))
+          }
+        } else {
+          console.log(res)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+    // setHomeData(JSON.parse(localStorage.getItem('nf-home')))
+  }, [])
+
+  return (
+    <>
+      <div className='lg:p-12'>
+        <section className='header flex justify-center mb-20'>
+          <h1 className='font-bold text-2xl lg:mt-0 mt-3'>Todays Top News</h1>
+        </section>
+        {homeData.length > 0 ? (
+          <div
+            id='container'
+            style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'center',
+            }}
+          >
+            {homeData.map((data) => (
+              <div style={{ padding: '10px' }}>
+                <CardContianer
+                  section={data.section}
+                  multimedia={data.multimedia}
+                  title={data.title}
+                  abstract={data.abstract}
+                />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className='text-center'>loading...</p>
+        )}
+      </div>
+    </>
+  )
 }
 
 export default Home
