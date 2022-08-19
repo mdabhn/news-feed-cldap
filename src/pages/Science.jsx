1561edcd4354c65a510c7c2dfec3032c0431ed08
Hossain <md.abhn@gmail.com>
2022-08-20 00:47:56 +0600

    Card Container updated.

--- src/pages/Science.jsx
+++ src/pages/Science.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import CardContianer from '../components/card/CardContianer'
 
 const Science = () => {
-  return <div>Science</div>
+  const [scienceData, setScienceData] = useState([])
+
+  useEffect(() => {
+    fetch(
+      `https://api.nytimes.com/svc/topstories/v2/science.json?api-key=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        if (res?.status) {
+          console.log(res)
+          if (res.status === 'OK') {
+            setScienceData(res.results)
+            localStorage.setItem('nf-us', JSON.stringify(res.results))
+          }
+        } else {
+          console.log(res)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+    // setScienceData(JSON.parse(localStorage.getItem('nf-us')))
+  }, [])
+
+  return (
+    <>
+      <div className='lg:p-12'>
+        <section className='header flex justify-center mb-20'>
+          <h1 className='font-bold text-2xl lg:mt-0 mt-3'>
+            Todays Top US News
+          </h1>
+        </section>
+        {scienceData.length > 0 ? (
+          <div
+            id='container'
+            style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'center',
+            }}
+          >
+            {scienceData.map((data) => (
+              <div style={{ padding: '10px' }}>
+                <CardContianer
+                  section={data.section}
+                  multimedia={data.multimedia}
+                  title={data.title}
+                  abstract={data.abstract}
+                />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className='text-center'>loading...</p>
+        )}
+      </div>
+    </>
+  )
 }
 
 export default Science
--- src/pages/US.jsx
+++ src/pages/US.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import CardContianer from '../components/card/CardContianer'
 
 const US = () => {
-  return <div>US</div>
+  const [usData, setUSData] = useState([])
+
+  useEffect(() => {
+    fetch(
+      `https://api.nytimes.com/svc/topstories/v2/us.json?api-key=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        if (res?.status) {
+          console.log(res)
+          if (res.status === 'OK') {
+            setUSData(res.results)
+            localStorage.setItem('nf-us', JSON.stringify(res.results))
+          }
+        } else {
+          console.log(res)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+    // setUSData(JSON.parse(localStorage.getItem('nf-us')))
+  }, [])
+
+  return (
+    <>
+      <div className='lg:p-12'>
+        <section className='header flex justify-center mb-20'>
+          <h1 className='font-bold text-2xl lg:mt-0 mt-3'>
+            Todays Top US News
+          </h1>
+        </section>
+        {usData.length > 0 ? (
+          <div
+            id='container'
+            style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'center',
+            }}
+          >
+            {usData.map((data) => (
+              <div style={{ padding: '10px' }}>
+                <CardContianer
+                  section={data.section}
+                  multimedia={data.multimedia}
+                  title={data.title}
+                  abstract={data.abstract}
+                />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className='text-center'>loading...</p>
+        )}
+      </div>
+    </>
+  )
 }
 
 export default US
