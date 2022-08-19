51916aa86081b41b2b5c7f44dcd3a71f96ce8829
Hossain <md.abhn@gmail.com>
2022-08-19 21:51:04 +0600

    404 page added.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import US from './pages/US'
+import Arts from './pages/Arts'
+import Home from './pages/Home'
+import World from './pages/World'
+import Science from './pages/Science'
+import PageNotFound from './pages/404'
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/us' element={<US />} />
+          <Route path='/world' element={<World />} />
+          <Route path='/science' element={<Science />} />
+          <Route path='/arts' element={<Arts />} />
+          <Route path='/*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import four0four from '../assets/images/404.png'
 
 const PageNotFound = () => {
-  return <div>404</div>
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <img src={four0four} alt='page-not-found' />
+    </div>
+  )
 }
 
 export default PageNotFound
