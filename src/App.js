88c506e085228f9495239fd9f6facaa5e8fbd7ab
Hossain <md.abhn@gmail.com>
2022-08-19 22:08:41 +0600

    Navigation added.

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import Home from './pages/Home'
 import World from './pages/World'
 import Science from './pages/Science'
 import PageNotFound from './pages/404'
+import Navigation from './components/navigation/Navigation'
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Navigation />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/us' element={<US />} />
---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+const Navigation = () => {
+  return (
+    <Navbar bg='light' expand='lg'>
+      <Container fluid>
+        <Navbar.Brand href='/'>Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbarScroll' />
+        <Navbar.Collapse id='navbarScroll'>
+          <Nav
+            className='me-auto my-2 my-lg-0'
+            style={{ maxHeight: '200px' }}
+            navbarScroll
+          >
+            <Nav.Link>
+              <Link to={'/'}>Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to={'/us'}>US</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to={'/world'}>World</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to={'/science'}>Science</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to={'/arts'}>Arts</Link>
+            </Nav.Link>
+          </Nav>
+          <Form className='d-flex'>
+            <Form.Control
+              type='search'
+              placeholder='Search'
+              className='me-2'
+              aria-label='Search'
+            />
+            <Button variant='outline-success'>Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Navigation
