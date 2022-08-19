5e8eb80ae3e941f95992d8b3170e54a1a094ba55
Hossain <md.abhn@gmail.com>
2022-08-20 01:17:22 +0600

    Context Api Added.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import US from './pages/US'
@@ -9,20 +9,26 @@ import Science from './pages/Science'
 import PageNotFound from './pages/404'
 import Navigation from './components/navigation/Navigation'
 
+export const ContextApi = createContext()
+
 const App = () => {
+  const [searchContext, setSearchContext] = useState('')
+
   return (
     <>
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/us' element={<US />} />
-          <Route path='/world' element={<World />} />
-          <Route path='/science' element={<Science />} />
-          <Route path='/arts' element={<Arts />} />
-          <Route path='/*' element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <ContextApi.Provider value={{ searchContext, setSearchContext }}>
+        <BrowserRouter>
+          <Navigation />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/us' element={<US />} />
+            <Route path='/world' element={<World />} />
+            <Route path='/science' element={<Science />} />
+            <Route path='/arts' element={<Arts />} />
+            <Route path='/*' element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </ContextApi.Provider>
     </>
   )
 }
